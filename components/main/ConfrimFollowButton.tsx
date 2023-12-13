0e14198b6186acade4730ba4ef869a68e6d6a256
HAE2NI <hae0_02ni@ewhain.net>
2023-12-13 16:59:03 +0900

    [ design ] 맞팔 확인하기 버튼

---
+++ components/main/ConfrimFollowButton.tsx
@@ -0,0 +1,20 @@
+import styled from '@emotion/styled';
+
+export default function ConfrimFollowButton() {
+  return <Button>나의 맞팔 확인하러 가기</Button>;
+}
+
+const Button = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  width: 29.6rem;
+  height: 4.5rem;
+
+  color: ${({ theme }) => theme.color.white};
+
+  background-color: ${({ theme }) => theme.color.sub01};
+  border: 2px solid ${({ theme }) => theme.color.grey02};
+  border-radius: 5px;
+`;
