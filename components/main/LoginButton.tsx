1f6ef27ea982e30f9ff46f9792c25b5e007aab37
HAE2NI <hae0_02ni@ewhain.net>
2023-12-14 01:50:12 +0900

    [ design ] 로그인 버튼에 text 추가

--- components/main/LoginButton.tsx
+++ components/main/LoginButton.tsx
@@ -1,12 +1,27 @@
 import styled from '@emotion/styled';
+import { St } from '../../styles/common';
 
 export default function LoginButton() {
   return (
     <>
+      <Wrapper>
+        <St.Title>OR</St.Title>
+        <St.Subtitle>깃허브 로그인으로 간단하게 확인하기 ⬇️️</St.Subtitle>
+      </Wrapper>
       <ButtonContainer>깃허브 로그인</ButtonContainer>
     </>
   );
 }
+
+const Wrapper = styled.article`
+  display: flex;
+  flex-direction: column;
+  gap: 2.2rem;
+  align-items: center;
+
+  width: 19.8rem;
+  height: 4.8rem;
+`;
 
 const ButtonContainer = styled.button`
   display: flex;
