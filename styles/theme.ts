c143180b71c629cfa02f04440d92e02b531e699e
HAE2NI <hae0_02ni@ewhain.net>
2023-12-13 17:08:37 +0900

    [ init ] font styled 변경

--- styles/theme.ts
+++ styles/theme.ts
@@ -1,15 +1,15 @@
-import { Theme } from "@emotion/react";
+import { Theme } from '@emotion/react';
 
 const color = {
-  main01: "#f2f9f2",
-  sub01: "#b6bbc4",
-  sub02: "#31304D",
-  red: "#E66375",
-  green: "#2B8D6A",
-  grey01: "#CAC5CA",
-  grey02: "#82868D",
-  black: "#000000",
-  white: "#ffffff",
+  main01: '#FCF9F2',
+  sub01: '#b6bbc4',
+  sub02: '#31304D',
+  red: '#E66375',
+  green: '#2B8D6A',
+  grey01: '#CAC5CA',
+  grey02: '#82868D',
+  black: '#000000',
+  white: '#ffffff',
 };
 
 function FONT(size: number) {
@@ -21,9 +21,9 @@ function FONT(size: number) {
 }
 
 const font = {
-  title01: FONT(15),
-  subTitle01: FONT(12),
-  subTitle02: FONT(10),
+  title01: FONT(1.5),
+  subTitle01: FONT(1.2),
+  subTitle02: FONT(1.0),
 };
 
 const theme: Theme = {
