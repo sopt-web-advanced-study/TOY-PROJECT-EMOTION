dc2ccc362ff083a5178ecbe01db742b26e87c1c7
HAE2NI <hae0_02ni@ewhain.net>
2023-12-13 17:00:32 +0900

    [ design ] 깃허브 로그인 버튼 퍼블리싱

---
+++ components/main/LoginButton.tsx
@@ -0,0 +1,24 @@
+import styled from '@emotion/styled';
+
+export default function LoginButton() {
+  return (
+    <>
+      <ButtonContainer>깃허브 로그인</ButtonContainer>
+    </>
+  );
+}
+
+const ButtonContainer = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  width: 14.1rem;
+  height: 3rem;
+
+  color: ${({ theme }) => theme.color.white};
+
+  background-color: ${({ theme }) => theme.color.sub02};
+  border: 1px solid ${({ theme }) => theme.color.black};
+  border-radius: 15px;
+`;
