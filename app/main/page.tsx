e59214511af2277154125bafc3d4ae245b18c103
HAE2NI <hae0_02ni@ewhain.net>
2023-12-14 15:34:16 +0900

    [ design ] 산타 모자 추가

--- app/main/page.tsx
+++ app/main/page.tsx
@@ -2,11 +2,13 @@
 
 import styled from '@emotion/styled';
 import MainContainer from '../../components/main/MainContainer';
+import Image from 'next/image';
 import { flexCenter } from '../../styles/common';
 
 export default function main() {
   return (
     <Wrapper>
+      <SantaHatImg src="/santahat.png" alt="산타모자" width={140} height={186} />
       <MainContainer />
     </Wrapper>
   );
@@ -15,6 +17,16 @@ export default function main() {
 const Wrapper = styled.section`
   ${flexCenter}
 
-  width:39rem;
-  height: 84rem;
+  flex-direction: column;
+  width: 100%;
+  height: 844px;
+`;
+
+const SantaHatImg = styled(Image)`
+  ${flexCenter}
+
+  position: absolute;
+  z-index: 1;
+  top: 10rem;
+  left: 10rem;
 `;
