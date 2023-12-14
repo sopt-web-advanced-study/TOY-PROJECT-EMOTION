a6abd1f0dbfc28ed45207e75dbc198c38a85f57d
HAE2NI <hae0_02ni@ewhain.net>
2023-12-14 15:23:08 +0900

    [ design ] Wrapper 추가

--- app/main/page.tsx
+++ app/main/page.tsx
@@ -1,9 +1,20 @@
+'use client';
+
+import styled from '@emotion/styled';
 import MainContainer from '../../components/main/MainContainer';
+import { flexCenter } from '../../styles/common';
 
 export default function main() {
   return (
-    <>
+    <Wrapper>
       <MainContainer />
-    </>
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.section`
+  ${flexCenter}
+
+  width:39rem;
+  height: 84rem;
+`;
